fca6e624c2aee5b5b23cd457e923cc226c527f3d
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-04 13:35:02 +0200

    Update bowtie.sh

--- Alignment/bowtie.sh
+++ Alignment/bowtie.sh
@@ -1,1 +1,47 @@
+#!/bin/bash
+#PBS -N bowtie 
+##PBS -j oe 
+##PBS -l file=100GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -o bowtie.stdout
+#PBS -e bowtie.stderr
 
+echo $PBS_JOBID
+echo $PBS_JOBNAME
+cd $PBS_O_WORKDIR
+
+name=$1
+
+index=~/igenome/Homo_sapiens/Ensembl/GRCh37/Sequence/Bowtie2Index
+out=~/chip_ana
+data=~/chip_data
+
+#flexbar -r wcc_tap_1.fastq -t wcc_tap_1_trimmed -z GZ -m 30 -a Adapter.fa
+#flexbar -r wcc_tap_1.fastq -t wcc_tap_1_trimmed -z GZ -m 30 -a adapter.fa
+
+bowtie2 -p 8 -x ${index} -sensitive-local -q -1 ${data}/${name}_1.fastq -2 ${data}/${name}_2.fastq -S ${out}/${name}.sam
+##convert sam file to bam 
+samtools view -bS ${out}/${name}.sam > ${out}/${name}.bam
+##sort bam file 
+samtools sort ${out}/${name}.bam >${out}/${name}_sort.bam
+##index bam file
+samtools index ${out}/${name}_sort.bam
+##convert bigwig file
+bamCoverage -b ${out}/${name}_sort.bam -o ${out}/${name}_sort.bigWig
+##convert bam file to bed file 
+bedtools bamtobed -${out}/${name}_sort.bam > ${out}/${name}_sort.bed
+
+
+
+
+
+
+######single end
+bowtie2 -p 8 -x ${index}/genome -sensitive-local -U ${data}/${name}.fastq -S ${out}/${name}.sam
+samtools view -bS ${out}/${name}.sam > ${out}/${name}.bam
+samtools sort ${out}/${name}.bam >${out}/${name}_sort.bam
+samtools index ${out}/${name}_sort.bam
+bedtools bamtobed -i ${out}/${name}_sort.bam >${out}/${name}_sort.bed
+bamCoverage -b ${out}/${name}_sort.bam -o ${out}/${name}_sort.bigWig
