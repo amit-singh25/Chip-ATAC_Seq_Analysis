2b4bc9efd31df71977a301b720f98931fa5afe9e
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-09 14:47:29 +0200

    Update Anototate.sh

--- Peak annotation/Anototate.sh
+++ Peak annotation/Anototate.sh
@@ -1,1 +1,24 @@
 
+#!/bin/bash
+#PBS -N macs
+##PBS -j oe 
+##PBS -l file=100GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -o macs.stdout
+#PBS -e macs.stderr
+
+name=$1
+
+out=/home/bq_asingh/chip_ana
+data=/home/bq_asingh/chip_data
+genome=/home/bq_asingh/genome
+## peak calling:
+
+
+#####peak anotate by homer 
+findMotifsGenome.pl ${name}.bed ${genome}/toplevel.fa OutputResults/
+
+####anotate peak by homer eith gene id
+annotatePeaks.pl ${name}.bed ${genome}/toplevel.fa -gtf ${genome}/gene.gtf -gid -hist 10 >peaks_annotate_gid.xls 
