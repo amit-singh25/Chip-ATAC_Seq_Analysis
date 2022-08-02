26582962af091bc650aa6f6f92f4a6030d2167ac
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2022-08-02 14:47:49 +0200

    Update macs.sh

--- Peak calling/macs.sh
+++ Peak calling/macs.sh
@@ -7,16 +7,15 @@
 #PBS -l nodes=1:ppn=8   
 #PBS -o macs.stdout
 #PBS -e macs.stderr
-
 name=$1
-
 out=/home/bq_asingh/chip_ana
 data=/home/bq_asingh/chip_data
+
 ## peak calling:
 
 macs2 callpeak -t ${data}/treat_sort.bam -c ${data}/control_sort.bam -f BAM -g mm B -q 0.01 --outdir macs2 -n test -
 
 ##broad peak calling: 
-macs2 callpeak -t ${data}/treat_sort.bam -c ${data}/control_sort.bam --broad -g mm --broad-cutoff 0.1 --outdir macs2 -n test_brod
 
+macs2 callpeak -t ${data}/treat_sort.bam -c ${data}/control_sort.bam --broad -g mm --broad-cutoff 0.1 --outdir macs2 -n test_brod
 macs14 callpeak -t ${data}/treat_sort.bam -c ${data}/control_sort.bam -g mm -p 1e-6 --outdir macs2 -n test > MACS.out
